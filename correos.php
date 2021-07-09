ae215f45882e0fd88d22712912ef1f82b1796318
Edwin Gallardo Ordóñez <edwingallardo516@gmail.com>
2021-07-08 21:32:03 -0500

    phpMailer

--- correos.php
+++ correos.php
@@ -1,22 +1,42 @@
 <?php
-$name = $_POST['nombre'];
-$mail = $_POST['mail'];
-$message = $_POST['mensaje'];
+$email = $_POST["mail"];
+$name = $_POST["nombre"];
+$message = $_POST["mensaje"];
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
+require 'phpmailer/PHPMailer/src/Exception.php';
+require 'phpmailer/PHPMailer/src/PHPMailer.php';
+require 'phpmailer/PHPMailer/src/SMTP.php';
 
-$para = 'edwingallardo516@gmail.com';
-$asunto = 'Mensaje de pagina web';
+$mail = new PHPMailer(true);
 
-mail($para, $asunto, utf8_decode($message), $header);
 
-header("Location:index.html");
-?>
+//Server settings
+$mail->SMTPDebug = 0;                      //Enable verbose debug output
+$mail->isSMTP();                                            //Send using SMTP
+$mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+$mail->Username   = 'edwingallardo5162@outlook.com';                     //SMTP username
+$mail->Password   = '2ce2870cb';                               //SMTP password
+$mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+$mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+//Recipients
+$mail->setFrom('edwingallardo5162@outlook.com', 'Mailer');
+$mail->addAddress('edwinpga@unicauca.edu.co', 'Mailer');     //Add a recipient
+
+//Formar mensaje
+$mensaje = 'Nombre: ' . strval($name) . '<br />' . 'Correo: ' . strval($email) . '<br />' . 'Mensaje: ' . '<br />' . $message;
+
+//Content
+$mail->isHTML(true);                                  //Set email format to HTML
+$mail->Subject = 'De Portafolio';
+$mail->Body    = $mensaje;
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+$mail->send();
+
+echo "<script> alert('Mensaje enviado') </script>";
+echo "<script> setTimeout(\"location.href='index.php'\",10) </script>";
